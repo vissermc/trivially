d887a4c689dfa3ae1ae341469866c7e42bd94d8b
Michiel Visser <michiel.visser@raith.com>
2025-11-07 09:54:04 +0100

    fix integration test

--- src/test/java/com/vissermc/trivially/FetcherIntegrationTest.java
+++ src/test/java/com/vissermc/trivially/FetcherIntegrationTest.java
@@ -5,29 +5,25 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Tag("external")
 @EnabledIfEnvironmentVariable(named = "RUN_EXTERNAL_TESTS", matches = "true")
 @SpringBootTest
-public class OpenTDBFetcherTest {
+public class FetcherIntegrationTest {
     @Autowired
-    OpenTDBFetcher fetcher;
+    TriviaServiceImpl service;
     @Test
-    void shouldCreateFetcherAndRetrieveQuestions() {
-        List<OpenTDBFetcher.OpenTriviaQuestion> questions = fetcher.fetchQuestionsAndAnswers();
+    void fetchesActualQuestions() {
+        var questions = service.getQuestions();
 
         // Assert: we got a non-empty list with basic fields present
         assertNotNull(questions, "Questions list should not be null");
         assertFalse(questions.isEmpty(), "Questions list should not be empty");
         var q = questions.getFirst();
         assertNotNull(q.question(), "Question text should be present");
-        assertNotNull(q.correct_answer(), "Correct answer should be present");
-        assertNotNull(q.incorrect_answers(), "Incorrect answers should be present");
-        assertFalse(q.incorrect_answers().isEmpty(), "There should be at least one incorrect answer");
+        assertNotNull(q.options(), "Options should not be null");
+        assertFalse(q.options().isEmpty(), "Options should not be empty");
     }
 }
